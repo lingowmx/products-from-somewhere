1b45c18d0ee65a0dde9716d6cc09db3b1d7f576a
lingowmx <clingow@hotmail.com>
2023-03-05 10:51:38 -0500

    objectlitarls with the name garageproducts

---
+++ garageProducts.js
@@ -0,0 +1,53 @@
+const bicycle = {
+    id:'bicycle',
+    name: ' Bicycle2000',
+    image: 'assets/bicycle.jpg',
+    description: 'A really nice bike',
+    category: 2,
+    price: '$90.00'
+}
+
+const monocycle = {
+    id:'monocycle',
+    name: 'Monocycle2000',
+    image: 'assets/monocycle.jpg',
+    description: 'A one wheel transport',
+    category: 1,
+    price: '$45.00'
+}
+const scooter = {
+    id:'scooter',
+    name: 'Scooter2000',
+    image: 'assets/scooter.jpg',
+    description: 'very smooth rides',
+    category: 3,
+    price: '$25.00'
+}
+const grandmaVan = {
+    id:'grandmaVan',
+    name: 'GrandmaVan1990',
+    image: 'assets/van.jpg',
+    description: 'Old but smells like new',
+    category: 4,
+    price: '$120.00'
+}
+const skaters = {
+    id:'skaters',
+    name: 'Skaters2000',
+    image: 'assets/skates.jpg',
+    description: 'Ready to go',
+    category: 3,
+    price: '$70.00'
+}
+const threeBike = {
+    id:'threeBike',
+    name: 'Threebike2000',
+    image: '🚲',
+    description: 'Just make it three',
+    category: 2,
+    price: '$180.00'
+}
+
+export default const myGarage = [bicycle, monocycle, scooter, grandmaVan, skaters, threeBike];
+
+
